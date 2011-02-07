40972c18e252f821183ce91a7c1b96f6294bb983
Marian Theisen <marian@cice-online.net>
2011-02-07 23:26:10 +0100

    extracted condition normalizer

---
+++ lib/kangaroo/model/condition_normalizer.rb
@@ -0,0 +1,53 @@
+module Kangaroo
+  module Model
+    module ConditionNormalizer
+      CONDITION_OPERATORS = ['not in', *%w(= != > >= < <= ilike like in child_of parent_left parent_right)].freeze
+      CONDITION_PATTERN = /\A(.*)\s+(#{CONDITION_OPERATORS * "|"})\s+(.*)\Z/i.freeze
+      
+      protected
+      def normalize_conditions conditions
+        conditions = Hash === conditions ? [conditions] : Array(conditions)
+        
+        conditions.map do |condition|
+          normalize_condition condition
+        end
+      end
+      
+      def normalize_condition condition
+        case condition
+        when Array
+          condition
+        when Hash
+          convert_hash_condition condition
+        when String
+          convert_string_condition condition
+        else
+          raise "Expected Array, Hash or String"
+        end
+      end
+      
+      def convert_hash_condition condition
+        condition.sum([]) do |key_val|
+          convert_key_value_condition *key_val
+        end
+      end
+      
+      def convert_key_value_condition field, value
+        operator = if Array === value
+          value = value.map &:to_s
+          'in'
+        else
+          value = value.to_s
+          '='
+        end
+
+        [field.to_s, operator, value]
+      end
+      
+      def convert_string_condition string
+        CONDITION_PATTERN.match(string).try(:captures) || []
+      end
+
+    end
+  end
+end
