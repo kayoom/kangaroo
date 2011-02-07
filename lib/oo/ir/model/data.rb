4990e33036a9110fe09324b8900c173766e92c13
Marian Theisen <marian@cice-online.net>
2011-02-07 16:31:55 +0100

    get object by reference

--- lib/oo/ir/model/data.rb
+++ lib/oo/ir/model/data.rb
@@ -1,3 +1,12 @@
-# class Oo::Ir::Model::Data < Kangaroo::Base
-#   
-# end
+class Oo::Ir::Model::Data < Kangaroo::Base
+  def self.get_object_reference idref
+    idref = idref.split '.'
+    database.execute self, 'get_object_reference', *idref
+  end
+  
+  def self.get_object_by_reference idref
+    type, id = get_object_reference idref
+    
+    Oo.class_for(type).find id
+  end
+end
