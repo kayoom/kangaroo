67875c70544510549526b232431835f3935dcb1f
Marian Theisen <marian@cice-online.net>
2011-04-30 10:22:04 +0200

    features war wohl echt zuviel des guten, rspec reicht

--- features/env.rb
+++
@@ -1,8 +0,0 @@
-require 'rubygems'
-require 'bundler/setup'
-
-require 'rspec/expectations'
-require 'cucumber/rspec/doubles'
-
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), *%w(.. .. lib)))
-require 'kangaroo'
