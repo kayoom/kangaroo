9ff1f9339d3bbfe8c3892c05b2f05338c3c24e99
Marian Theisen <marian@cice-online.net>
2012-08-27 12:53:58 +0200

    support for nested root namespace

--- lib/kangaroo/util/loader/namespace.rb
+++ lib/kangaroo/util/loader/namespace.rb
@@ -5,18 +5,20 @@ module Kangaroo
         def inspect
           "Module '#{name}' contains loaded OpenERP Models/Namespaces: #{constants.join(', ')}"
         end
-      
+
         def namespace_name
           name.match(/^([^\:]+)/)[1]
         end
-      
+
         def namespace
           namespace_name.constantize
         end
-      
+
         def const_missing const_name
           namespaced_const_name = "#{name}::#{const_name}"
-        
+
+          return super unless namespace.respond_to?(:model_exists?)
+
           if namespace.model_exists?(namespaced_const_name)
             namespace.load_model(namespaced_const_name)
           elsif namespace.models_in?(namespaced_const_name)
